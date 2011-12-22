97520e87ad50ce422ad53ce1e4f604e5250ebac0
MatToufoutu <mattoufootu@gmail.com>
2011-12-22 15:07:02 +0100

    create the setup script

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read().strip()
+DEPENDENCIES = [
+    'distribute',
+    'PasteScript>=1.3',
+    'Cheetah',
+    'fabric',
+]
+
+
+setup(
+    name = 'flask-project-templates',
+    version = '0.1',
+    description = 'Paster templates for creating Flask projects',
+    long_description = README,
+    author = 'Mathieu D. (MatToufoutu)',
+    author_email = 'mattoufootu@gmail.com',
+    url = 'https://github.com/mattoufoutu/flask-project-templates',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    zip_safe = False,
+    license = 'GPL',
+    keywords = 'flask paster template',
+    install_requires = DEPENDENCIES,
+    classifiers = [
+        'Development Status :: 1 - Planning',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independant',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Software Development :: Code Generators',
+    ],
+    entry_points = """
+    """,
+)
